2f64c536ff3f277b72cfb67400cb527cf0182d74
Hitesh mishra <mishrahitesh854@gmail.com>
2023-12-17 21:56:42 +0530

    10

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import User from "../../../../models/user";
 import { connectToDb } from "@/utils/database";
 
-export const maxDuration = 15; // This function can run for a maximum of 5 seconds
+export const maxDuration = 10; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
 
 
